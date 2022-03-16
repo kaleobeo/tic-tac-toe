99377f18ec01b1211269e9b2e50b7561cf2c93cb
Kaleo K <kaleo.kronberg@gmail.com>
2022-03-15 17:35:38 -0700

    Add functionality to get and set legal symbols

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
+# Mangaes gameboard values
 class GameBoard
+  DEFAULT_BOARD = {
+    a: [' ', ' ', ' '],
+    b: [' ', ' ', ' '],
+    c: [' ', ' ', ' ']
+  }.freeze
   def initialize
-    @board = {
-      a: [' ', ' ', ' '],
-      b: [' ', ' ', ' '],
-      c: [' ', ' ', ' ']
-    }
+    
   end
 
   def to_s
@@ -19,8 +21,23 @@ class GameBoard
       end
     end
   end
-
 end
 
 class Player
+  @@legal_symbols = %w[x o]
+  attr_reader :symbol
+
+  def initialize
+    puts 'Please enter "x" or "o" for this player.'
+    @symbol = gets.chomp
+    until @@legal_symbols.include?(@symbol)
+      puts 'Please enter a valid symbol.'
+      @symbol = gets.chomp
+    end
+    @@legal_symbols.delete(@symbol)
+  end
+
+  def self.legal_symbols
+    @@legal_symbols
+  end
 end
